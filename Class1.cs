f26869259f4a3b47562d66143be7c8282bf23cf1
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2021-06-16 19:25:19 -0400

    yes

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Reflection;
+using HarmonyLib.Tools;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace W_jump
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInProcess("Rounds.exe")]
+    [BepInPlugin("uniquename.rounds.zz-comic-mode", "zz-comic-mode", "0.0.0.0")]
+    public class W_Jump : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            var harmony = new Harmony("uniquename.rounds.zz-comic-mode");
+            harmony.PatchAll();
+        }
+
+        public const string modID = "uniquename.rounds.zz-comic-mode";
+        public const string modName = "zz-comic-mode";
+    }
+
+    [HarmonyPatch(typeof(Block), "TryBlock")]
+    public class zz_comic_Patch
+    {
+        public static bool Prefix(Block __instance)
+        {
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+disables blocking
+
+just put zz-comic-mode.dll into your plugins folder to use the mod
